a860490088b62a875b129a9f2b662ec568cfdb4f
john <borges-2014@live.com>
2020-10-24 17:56:57 -0300

    [WIP] add files for tokenizing numbers

---
+++ tests/testNumbers.java
@@ -0,0 +1,15 @@
+import junit.framework.*;
+import junit.textui.*;
+
+
+public class NumberTestCase extends TestCase {
+    protected void setUp() {
+        return;
+    }
+
+   @Test
+   public void test_tokenizer() {
+
+   }
+
+}
