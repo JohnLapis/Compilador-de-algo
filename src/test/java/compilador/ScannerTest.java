e19fabaca9523b50eacc67e6a0697f14aadd023f
john <borges-2014@live.com>
2020-10-25 17:38:37 -0300

    [WIP] Test: add XMLMapper for loading fixtures

--- src/test/java/compilador/ScannerTest.java
+++ src/test/java/compilador/ScannerTest.java
@@ -1,9 +1,11 @@
+// [[file:ScannerTest.org::*imports][imports:1]]
 package compilador;
 
 import java.util.Arrays;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Collection;
+import org.junit.BeforeClass;
 
 import junitparams.*;
 import org.junit.Test;
@@ -13,13 +15,45 @@ import junitparams.naming.TestCaseName;
 
 import static junitparams.JUnitParamsRunner.*;
 import static org.junit.Assert.assertEquals;
+// imports:1 ends here
 
+// [[file:ScannerTest.org::*Mapper][Mapper:1]]
+class XMLMapper implements DataMapper {
+    static Object[] map(Reader reader) {
+        Document data = DocumentBuilder.parse(reader.read());
+        return getElementsByTagName("codeSamples")[0]
+            .getElementsByTagName("code")
+            .stream()
+            .map((element) -> getTextContent(element));
+     }
+}
+
+// Mapper:1 ends here
+
+// [[file:ScannerTest.org::*ScannerTest][ScannerTest:1]]
 @RunWith(JUnitParamsRunner.class)
 public class ScannerTest {
+    private Path fixtureDirectory = Path.get("fixtures");
+
+    @BeforeClass
+    private void setUp() {
+        // for file in directory x
+        //              load file into property
+
+
+        //              and then make _____Values() use the attribute
+        // for (Path filename : directory.listFiles()) {
+        //     this.filename = loadFile(filename);
+        // }
+
+    }
 
     @Test
-    @Parameters(method = "arithmeticValues")
+    @FileParameters(value = "src/test/java/compilador/fixtures/expression.xml", mapper = XMLMapper.class)
     public void testTokenzine(String input, List<String> expected) {
+        System.out.println(input);
+        System.out.println(expected);
+
         try {
             assertEquals(Scanner.tokenize(input), expected);
         } catch (Throwable e) {
@@ -27,13 +61,5 @@ public class ScannerTest {
             e.printStackTrace();
         }
     }
-
-    public static Collection<Object[]> arithmeticValues() {
-        return Arrays.asList(new Object[][] {
-                { "44 + 845", Arrays.asList("44", "+", "845") },
-                { "44 + 845 * (885 - 33 / 5) ** 9", Arrays.asList("44", "+", "845", "*", "(", "885", "-", "33", "/", "5", ")", "**", "9") },
-                { "44 + a * (b) ** 9", Arrays.asList("44", "+", "a", "*", "(", "b", ")", "**", "9") }
-            });
-    }
-
 }
+// ScannerTest:1 ends here
