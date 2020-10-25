1e6f973772025ccd752d0b147836c0bf74febe39
john <borges-2014@live.com>
2020-10-25 02:23:45 -0300

    [WIP] Test: Add parameterization for TestScanner

--- src/test/java/compilador/TestScanner.java
+++ src/test/java/compilador/TestScanner.java
@@ -1,13 +1,58 @@
 package compilador;
 
 import org.junit.Test;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 import java.util.Arrays;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+import java.util.List;
+import java.util.ArrayList;
+import org.junit.runners.Suite;
+import java.util.Collection;
 
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        TestArithmetic.class,
+            })
 public class TestScanner {
+
+}
+
+@RunWith(Parameterized.class)
+class TestArithmetic {
+    @Parameters
+    public static List data() {
+        List parameters =
+            Arrays.asList(
+                          Arrays.asList("44 + 845", Arrays.asList("44", "845")),
+                          Arrays.asList("44 + 845 * (885 - 33 / 5) ** 9", Arrays.asList("44", "+", "845", "*", "(", "885", "-", "33", "/", "5", ")", "9")),
+                          Arrays.asList("44 + a * (b) ** 9", Arrays.asList("44", "+", "a", "*", "(", "b", "-", "33", "/", "5", ")", "9"))
+                          );
+        return parameters;
+    }
+
+    @Parameter
+    public String input;
+
+    @Parameter(1)
+    public List<String> expected = new ArrayList<String>();
+
+    public TestArithmetic(String input, List<String> expected) {
+        this.input = input;
+        this.expected = expected;
+    }
+
     @Test
-    public void number() {
-        String params[] = {"44 + 856", "60", ""};
-        assert(tokenize("44 + 845") == {"44", "845"})
+    public void testArithmetic() {
+        System.out.println(input);
+        try {
+            assertEquals(Scanner.tokenize(input), expected);
+        } catch (Throwable e) {
+            System.out.println("Error " + e.getMessage());
+            e.printStackTrace();
+        }
     }
 }
