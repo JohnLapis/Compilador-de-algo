36a708152983e7dd56f99741b68dfa768ec46da9
john <borges-2014@live.com>
2020-10-24 22:31:24 -0300

    squash! [WIP] Scanner recognizes numbers

--- src/main/java/compilador/Scanner.java
+++ src/main/java/compilador/Scanner.java
@@ -6,44 +6,42 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-
 // imports:1 ends here
 
 // [[file:Scanner.org::*number][number:1]]
 class Scanner {
-  public enum Keywords {};
+    public enum Keywords {};
 
-  public static List<String> tokenize(String code) throws ParseException {
-    Pattern pattern = Pattern.compile("\\d+");
-    Matcher matcher = pattern.matcher(code);
-    List<String> matches = new ArrayList<String>();
-    while (matcher.find()) {
-      matches.add(matcher.group());
+    public static List<String> tokenize(String code) throws ParseException {
+        Pattern pattern = Pattern.compile("\\d+");
+        Matcher matcher = pattern.matcher(code);
+        List<String> matches = new ArrayList<String>();
+        while (matcher.find()) {
+            matches.add(matcher.group());
+        }
+        return matches;
     }
-    if (matches.length == 0) {
-        return ParseException;
-    }
-    return matches;
-  }
+    // number:1 ends here
 
-  public static void main(String[] args) {
-    // It will only accept one file.
-    if (args.length != 1) {
-      System.out.println("Wrong number of parameters provided.");
-      return;
-    }
+    // [[file:Scanner.org::*number][number:2]]
+    public static void main(String[] args) {
+        // It will only accept one file.
+        if (args.length != 1) {
+            System.out.println("Wrong number of parameters provided.");
+            return;
+        }
 
-    Path path = Paths.get(args[0]);
-    String code;
-    try {
-        code = String.join("\n", Files.readAllLines(path));
-    } catch (IOException e) {
-        System.out.println("Error " + e.getMessage());
-        e.printStackTrace();
-        return;
-    }
-    System.out.println("[ " + String.join(", ", tokenize(code)) + " ]");
+        Path path = Paths.get(args[0]);
+        String code;
+        try {
+            code = String.join("\n", Files.readAllLines(path));
+        } catch (IOException e) {
+            System.out.println("Error " + e.getMessage());
+            e.printStackTrace();
+            return;
+        }
+        System.out.println("[ " + String.join(", ", tokenize(code)) + " ]");
 
-  }
+    }
 }
-// number:1 ends here
+// number:2 ends here
